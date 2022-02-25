8d7b08bd234a3dd25acacc97b15e312214415a3d
HaroldEstn <hgamboarodr@uniminuto.edu.co>
2022-02-25 16:17:43 -0500

    version 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    HobbiesComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'skills', component: SkillsComponent},
+      {path: 'projects', component: ProjectsComponent},
+      {path: 'hobbies', component: HobbiesComponent},
+      {path: '**', component: PageNotFoundComponent},
+    ]),
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
